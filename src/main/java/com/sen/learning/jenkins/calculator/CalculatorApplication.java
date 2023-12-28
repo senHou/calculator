a4e0b664cd0b64ae951c9c93236aefbdec0a9f84
shou <sen.hou1987@gamil.com>
2023-12-29 10:59:24 +1100

    create a spring boot app

---
+++ src/main/java/com/sen/learning/jenkins/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.sen.learning.jenkins.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
